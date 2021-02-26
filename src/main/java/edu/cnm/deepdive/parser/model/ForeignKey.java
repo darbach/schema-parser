b5ccf95f1715adf933f425bb688e3218650409c2
darbach <danielarbach@gmail.com>
2021-02-26 10:31:14 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/parser/model/ForeignKey.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.parser.model;
+
+public class ForeignKey {
+
+  // TODO Add fields, getters & setters, and @Expose annotations.
+
+}
---
+++ src/main/java/edu/cnm/deepdive/parser/model/PrimaryKey.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.parser.model;
+
+public class PrimaryKey {
+
+  // TODO Add fields, getters & setters, and @Expose annotations.
+
+}
